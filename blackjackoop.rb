529e27fcbb189266c3002525c58be708595369d2
Mark Hinschberger <markhinschberger@gmail.com>
2015-06-09 16:39:10 +0800

    updated bug fixes

--- blackjackoop.rb
+++ blackjackoop.rb
@@ -4,6 +4,8 @@
 # 3. Extract major verbs -> instance methods
 # 4. Group instance methods into classes
 
+
+#has a relationship => composition , achieve this through using a module.
 # there is a dealer and a player. each are given two cards. the player has the chance to hit or stay. if the dealer gets 21 the player loses
 require 'pry'
 #dealer, player, cards, deck, 
@@ -52,30 +54,13 @@ end
   end
 end
 
-#game engine
-  class Blackjack
-    def initialize
-      @player = Player.new
-      @dealer = Dealer.new
-      @deck = Deck.new
-  end
-
-  def run
-    deal_cards
-    show_flop
-    player_turn
-    dealer_turn
-    who_won?
-  end
-end
-
 
 module Playcards
   
   def show_hand
     puts " ====> #{name}' cards"
     cards.each do |card|
-      puts "=> #{card}"
+    puts "=> #{card}"
   end
     puts "=> Total: #{total}"
 
@@ -83,7 +68,7 @@ module Playcards
 
 
 
- def total
+   def total
     value = cards.map{|card| card.value }
 
     total = 0
@@ -112,7 +97,6 @@ module Playcards
 
   def add_card(new_card)
     cards << new_card
-
   end
 end
 
@@ -131,6 +115,7 @@ class Player
   end
 end   
 
+
 class Dealer
   include Playcards
   attr_accessor :name, :cards
@@ -148,6 +133,7 @@ class Dealer
 end
 
 
+
 class Blackjack
   attr_accessor :deck, :player, :dealer
   
@@ -156,7 +142,7 @@ class Blackjack
   
   def initialize 
     @deck = Deck.new
-    @player = Player.new('Mark')
+    @player = Player.new("mark")
     @dealer = Dealer.new
   end
 
@@ -164,6 +150,7 @@ class Blackjack
     puts " whats your name?"
     player.name = gets.chomp
   end
+
 
   def deal_cards
     player.add_card(deck.deal)
@@ -178,44 +165,42 @@ class Blackjack
   end
 
   def blackjack_or_bust?(player_or_dealer)
-    if player_turn.total == BLACKJACK_AMOUNT
+    if player_or_dealer.total == BLACKJACK_AMOUNT
       if player_or_dealer.is_a?(Dealer)
-        puts " The dealer hit blackjack, #{player.name} lost"
+        puts "Sorry, dealer hit blackjack. #{player.name} loses."
       else
-        puts "congratulations, you hit blackjack, #{player.name} win"
+        puts "Congratulations, you hit blackjack! #{player.name} win!"
       end
-      exit
-
+      continue_playing?
     elsif player_or_dealer.is_busted?
       if player_or_dealer.is_a?(Dealer)
-        puts "congratulations, dealer busted. #{player.name} win"
+        puts "Congratulations, dealer busted. #{player.name} win!"
       else
-        puts "Sorry, #{player.name} busted. #{player.name} losses"
+        puts "Sorry, #{player.name} busted. #{player.name} loses."
       end
-     exit
-   end
+    end
+  end
   
-
-
   def player_turn
     puts "#{player.name}" 's turn'
-     blackjack_or_bust?(player)
+     
+      blackjack_or_bust?(player)
+      
       while !player.is_busted?
        puts " Hit or stay. 1)Hit  2) Stay"
        response = gets.chomp
 
       if !['1','2'].include?(response)
         puts "Error: You must select 1 or 2"
-    next 
-  end
+        next
+      end 
+
+      if response == "2"
+        puts "#{player.name} chose to stay"
+      break
+   end
 
 
-
-  if response == "2"
-    puts "#{player.name} chose to stay"
-   break
-  end
-end
 
   #hit
   new_card = deck.deal
@@ -223,37 +208,54 @@ end
   player.add_card(new_card)
   puts "#{player.name} total is now : #{player.total}"
 
-  blackjack_or_bust?(player)
+ 
+
+  end
+   
+  puts " #{player.name} stays at #{player.total}"
 end
 
 
-puts " #{player.name} stays at #{player.total}"
+  def dealer_turn
+    puts "Dealer turn"
+    blackjack_or_bust?(dealer)
+  while dealer.total < DEALER_HIT_MIN 
+    new_card = deck.deal
+    puts "dealing card to dealer : #{new_card}"
+    dealer.add_card(new_card)
+    puts "dealer total is now #{dealer.total}"
+
+
+    blackjack_or_bust?(dealer)
+  end
+  puts "dealer stays at #{dealer.total}"
+ end
+
+  def who_won?
+    if player.total>dealer.total
+      puts "congratulations #{player.name} wins"
+    elsif player.total<dealer.total 
+      puts "sorry the dealer one"
+    else
+      puts "its a tie!"
+  end
+  
+  def continue_playing?
+    puts "would you like to play again? 1) yes 2) no"
+    play_again = gets.chomp
+    if play_again == '1'
+    deck = Deck.new
+    player.cards =[]
+    dealer.cards = []
+    start
+  else
+    puts "thanks for playing"
+    exit
+  end
 end
 
-# def dealer_turn
-#   puts "Dealer turn"
-#   blackjack_or_bust?(dealer)
-#   while dealer.total < DEALER_HIT_MIN 
-#   new_card = deck.deal
-#   puts "dealing card to dealer : #{new_card}"
-#   dealer.add_card(new_card)
-#   puts "dealer total is now #{dealer.total}"
-
-
-#   blackjack_or_bust?(dealer)
-#   end
-# puts "dealer stays at #{dealer.total}"
-# end
-
-# def who_won?
-#   if player.total>dealer.total
-#     puts "congratulations #{player.name} wins"
-#   elsif player.total<dealer.totalputs 
-#     puts "sorry the dealer one"
-#   else
-#     puts "its a tie!"
-#   end
-# end
+   
+end
 
 
 
@@ -262,23 +264,18 @@ end
     deal_cards
     show_flop
     player_turn
-    # dealer_turn
-    # who_won?(player,dealer)
+    dealer_turn
+    who_won?
+    continue_playing?
   end
 
-
 end
+
+
 
 game = Blackjack.new
 game.start
 
 
-#has a relationship => composition , achieve this through using a module.
 
 
-# a=Card.new("H",'4')
-# puts a
-
-
-# puts deck.cards
-# puts deck.cards.size
